9a8b24cb69b4a765c6eb3ef9636b698628ed2411
manueldiagostino <diagostinomanuel@gmail.com>
2022-11-26 12:49:37 +0100

    added some info

--- .config/wofi/po.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
-
-selected=$(echo -e "$entries" | wofi -i -d --prompt "Power menu" --location 1 --xoffset 15 --yoffset 15 --width 250 --lines 5 | awk '{print tolower($2)}')
-
-echo "$selected"
