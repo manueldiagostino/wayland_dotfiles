9d81754c996f758025fb506bd949e2bcb39bba6f
manueldiagostino <diagostinomanuel@gmail.com>
2022-12-18 18:29:39 +0100

    Improves low battery notification

--- .config/hypr/handle_monitor_connection.sh
+++ .config/hypr/handle_monitor_connection.sh
@@ -3,8 +3,28 @@
 function handle {
 	if [[ ${1:0:12} == "monitoradded" ]]; then
     	hyprctl dispatch dpms off eDP-1
+		hyprctl dispatch moveworkspacetomonitor "1 1"
+		hyprctl dispatch moveworkspacetomonitor "2 1"
+		hyprctl dispatch moveworkspacetomonitor "4 1"
+		hyprctl dispatch moveworkspacetomonitor "5 1"
+		hyprctl dispatch moveworkspacetomonitor "6 1"
+		hyprctl dispatch moveworkspacetomonitor "3 0"
+		hyprctl dispatch moveworkspacetomonitor "7 0"
+		hyprctl dispatch moveworkspacetomonitor "8 0"
+		hyprctl dispatch moveworkspacetomonitor "9 0"
+		hyprctl dispatch moveworkspacetomonitor "10 0"
 	elif [[ ${1:0:14} == "monitorremoved" ]]; then
 		hyprctl dispatch dpms on eDP-1
+		hyprctl dispatch moveworkspacetomonitor "1 0"
+		hyprctl dispatch moveworkspacetomonitor "2 0"
+		hyprctl dispatch moveworkspacetomonitor "4 0"
+		hyprctl dispatch moveworkspacetomonitor "5 0"
+		hyprctl dispatch moveworkspacetomonitor "6 0"
+		hyprctl dispatch moveworkspacetomonitor "3 0"
+		hyprctl dispatch moveworkspacetomonitor "7 0"
+		hyprctl dispatch moveworkspacetomonitor "8 0"
+		hyprctl dispatch moveworkspacetomonitor "9 0"
+		hyprctl dispatch moveworkspacetomonitor "10 0"
 	fi
 }
 
--- .config/hypr/start.sh
+++ .config/hypr/start.sh
@@ -7,8 +7,18 @@ if [ $ret -eq 0 ]
 then
 	# hyprctl keyword monitor DP-1,1920x1080@60,0x0,1,mirror,eDP-1 && \
 	hyprctl keyword monitor DP-1,1920x1080@60,1920x0,1 && \
-	hyprctl keyword wsbind 2,DP-1 && \
-	hyprctl dispatch dpms off eDP-1 
+	hyprctl keyword wsbind 1,DP-1 && \
+	hyprctl dispatch dpms off eDP-1
+	hyprctl dispatch moveworkspacetomonitor "1 1"
+	hyprctl dispatch moveworkspacetomonitor "2 1"
+	hyprctl dispatch moveworkspacetomonitor "4 1"
+	hyprctl dispatch moveworkspacetomonitor "5 1"
+	hyprctl dispatch moveworkspacetomonitor "6 1"
+	hyprctl dispatch moveworkspacetomonitor "3 0"
+	hyprctl dispatch moveworkspacetomonitor "7 0"
+	hyprctl dispatch moveworkspacetomonitor "8 0"	
+	hyprctl dispatch moveworkspacetomonitor "9 0"
+	hyprctl dispatch moveworkspacetomonitor "10 0"	
 fi
 
 exit 0
