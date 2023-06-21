97084b0cab1f715688360882fe0971482537f33a
manueldiagostino <diagostinomanuel@gmail.com>
2023-06-21 21:39:34 +0200

    Configurazione eww - Inserito controllo gamma in brightnessBox

---
+++ .config/eww/bar/scripts/gamma.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+val=$(busctl --user introspect rs.wl-gammarelay / rs.wl.gammarelay | grep ".Temperature" | tail -1 | awk '{print $4}')
+
+[ "$1" == "get" ] && \
+    if [[ $val -eq 6500 ]] 
+    then 
+        echo "" 
+    else 
+        echo ""
+    fi \
+    && exit
+
+[ "$1" == "set" ] && \
+if [[ $val -eq 6500 ]]
+then
+    busctl --user -- set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 4500
+else
+    busctl --user -- set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 6500
+fi \
+&& exit
