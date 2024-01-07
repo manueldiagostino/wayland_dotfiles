ea9c2a8fd59253e3a18a18f337782c5e09aba9e5
manueldiagostino <diagostinomanuel@gmail.com>
2024-01-07 09:48:28 +0100

    modifica monitor (spostato hdmi a sinistra del monitor pc)

--- .config/hypr/idle.sh
+++ .config/hypr/idle.sh
@@ -2,7 +2,18 @@
 
 swayidle -w \
 	timeout 900 '~/.config/hypr/lock.sh' \
-	timeout 1200 'hyprctl dispatch dpms off eDP-1 && hyprctl dispatch dpms off DP-1 && systemctl suspend' \
+	timeout 1200 '
+		hyprctl monitors | grep HDMI
+		ret=$?
+        
+        if [ $ret -eq 0 ]	
+		then
+			hyprctl dispatch dpms off DP-1
+		else
+			hyprctl dispatch dpms off eDP-1
+		fi
+
+        systemctl suspend' \
 	resume '
 		hyprctl monitors | grep HDMI
 		ret=$?
